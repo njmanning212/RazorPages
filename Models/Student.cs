39b511e92260002e8b8d924e704567c6c3984713
Nicholas Manning <njmanning212@gmail.com>
2023-10-27 11:55:52 -0500

    add models folder and course, student, and nrollment models

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity;
+
+public class Student
+{
+  public int ID { get; set; }
+  public string LastName { get; set; }
+  public string FirstName { get; set; }
+  public DateTime EnrollmentDate { get; set; }
+  public ICollection<Enrollment> Enrollments { get; set; }
+}
