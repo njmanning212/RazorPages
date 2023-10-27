6abf4161a543b511f3da730c2f328ca6253cb143
Nicholas Manning <njmanning212@gmail.com>
2023-10-27 14:54:43 -0500

    create CU.bd

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -14,6 +14,15 @@ namespace ContosoUniversity.Data
         {
         }
 
-        public DbSet<ContosoUniversity.Student> Student { get; set; } = default!;
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Course");
+        }
     }
 }
